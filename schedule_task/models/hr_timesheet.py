5bc3ac1389e593de89d91393f9a14afe68e533f9
zdni <alzidni.kasim@gmail.com>
2025-01-06 17:01:02 +0700

    set not required schedule

--- schedule_task/models/hr_timesheet.py
+++ schedule_task/models/hr_timesheet.py
@@ -3,4 +3,4 @@ from odoo import _, api, fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    schedule_id = fields.Many2one('schedule.task', string='Schedule', required=True)
+    schedule_id = fields.Many2one('schedule.task', string='Schedule')
