52ed07ea256c9f1fe48c94e198b6219e1002daf3
zdni <alzidni.kasim@gmail.com>
2025-01-08 13:21:52 +0700

    solve combination indices in import product variant

--- product_product_import/wizards/import_product_variant.py
+++ product_product_import/wizards/import_product_variant.py
@@ -36,6 +36,7 @@ class ImportProductVariant(models.TransientModel):
                     
                     variant_value_ids = []
                     variants = row_vals[2].split(' || ')
+                    combination_indices = []
                     for variant in variants:
                         attribute_name = variant.split(': ')[0]
                         variant_name = variant.split(': ')[1]
@@ -62,13 +63,14 @@ class ImportProductVariant(models.TransientModel):
                         if not product_template_variant_value_ids:
                             raise UserError(f"Product {row_vals[1]}, Attribute {attribute_name}, f{product_attribute_value.name}, product_template_variant_value_ids not Found")
                         variant_value_ids.append((4, product_template_variant_value_ids.id))
+                        combination_indices.append(product_template_variant_value_ids.id)
 
                     vals = {
                         'name': row_vals[0],
                         'product_tmpl_id': product_tmpl_id.id,
                         'product_template_variant_value_ids': variant_value_ids,
+                        'combination_indices': ','.join(combination_indices)
                     }
-                    _logger.warning(vals)
                     self.env['product.product'].create(vals)
 
         except UserError as e:
