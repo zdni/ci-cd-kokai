a3c16d5a8dbccb78648034be4d421b2a4176c8ab
zdni <alzidni.kasim@gmail.com>
2025-01-21 11:11:29 +0700

    logger in import inventory

--- inventory_adjustment_import/wizards/import_inventory_adjustment_wizards.py
+++ inventory_adjustment_import/wizards/import_inventory_adjustment_wizards.py
@@ -55,8 +55,8 @@ class ImportInventoryAdjustmentWizard(models.TransientModel):
             i = 0
             for rec in range(sheet.nrows):
                 i += 1
-                _logger.warning(i)
                 if rec >= 1:
+                    _logger.warning(i)
                     row_vals = sheet.row_values(rec)
                     if len(row_vals) < int(2):
                         raise UserError(_("Please ensure that you selected the correct file"))
