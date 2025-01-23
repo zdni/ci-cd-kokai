e6fd144ff14cb876992e9df80322f10ae1df638d
zdni <alzidni.kasim@gmail.com>
2025-01-23 10:49:40 +0700

    set setting read and write document related with parent folder

--- document_management_access/models/documents.py
+++ document_management_access/models/documents.py
@@ -6,3 +6,13 @@ class DocumentsFolder(models.Model):
 
     department_ids = fields.Many2many('hr.department', string='Write Departments', copy=True)
     read_department_ids = fields.Many2many('hr.department', 'documents_folder_read_departments', string='Read Departments', copy=True)
+
+    @api.onchange('parent_folder_id')
+    def _onchange_parent_folder_id(self):
+        for record in self:
+            if record.parent_folder_id:
+                record.department_ids = record.parent_folder_id.department_ids
+                record.read_department_ids = record.parent_folder_id.read_department_ids
+            else:
+                record.department_ids = []
+                record.read_department_ids = []
