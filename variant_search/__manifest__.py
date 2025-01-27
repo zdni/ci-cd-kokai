c25784da44415a94375198a015ba5424c88c8705
zdni <alzidni.kasim@gmail.com>
2025-01-27 12:43:28 +0700

    add variant search in product

---
+++ variant_search/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Released under LGPL v.3
+#
+##############################################################################
+
+{
+    "name": "Product variant search",
+    "version": "1.5",
+    "category": "Sales",
+    "description": "",
+    "author": "Luca Vercelli - Finsoft srl",
+    "website": "",
+    "depends": ["product"],
+    'data': [],
+    "init_xml": [],
+    "update_xml": [],
+    "demo_xml": [],
+    "test": [],
+    "installable": True,
+    "active": False,
+    "license" : 'LGPL-3',
+}
