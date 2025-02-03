1733cc1eb547074f22f482c966e38fc9b169706d
zdni <alzidni.kasim@gmail.com>
2025-02-03 08:40:20 +0700

    complete approval expense

--- approvals_expense/models/hr_expense.py
+++ approvals_expense/models/hr_expense.py
@@ -1,4 +1,13 @@
-from odoo import _, api, fields, models
+from odoo import _, api, fields, models, Command, _
+from odoo.exceptions import UserError
+from odoo.tools import email_split, float_is_zero, float_repr, float_compare, is_html_empty
+from odoo.tools.misc import clean_context, format_date
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
 
 
 class HrExpense(models.Model):
@@ -45,12 +54,12 @@ class HrExpense(models.Model):
             'state': 'request'
         })
         request = self.approval_ids[self.approval_count-1]
-        approver = self.env['approval.approver'].search([
-            ('request_id.id', '=', request.id),
-            ('user_id.id', '=', 2),
-        ])
-        if approver:
-            approver.sudo().write({ 'user_id': self.employee_id.parent_id.user_id.id })
+        # approver = self.env['approval.approver'].search([
+        #     ('request_id.id', '=', request.id),
+        #     ('user_id.id', '=', 2),
+        # ])
+        # if approver:
+        #     approver.sudo().write({ 'user_id': self.employee_id.parent_id.user_id.id })
         request.action_confirm()
 
     def action_view_approval_request(self):
@@ -80,7 +89,7 @@ class HrExpense(models.Model):
             'model': 'hr.expense',
             'res_id': self.id,
         })
-        self.action_submit_expenses()
+        self.with_context(forced_create=True).action_submit_expenses()
         self.sheet_id.write({ 'state': 'approve' })
         # self.sheet_id.write({ 'user_id': self.env.user.id })
         # self.sheet_id.action_submit_sheet()
@@ -107,4 +116,70 @@ class HrExpense(models.Model):
             'res_id': self.id,
         })
         notification.action_assign()
-    
+
+    def _get_default_expense_sheet_values(self):
+        # If there is an expense with total_amount_company == 0, it means that expense has not been processed by OCR yet
+        expenses_with_amount = self.filtered(lambda expense: not float_compare(expense.total_amount_company, 0.0, precision_rounding=expense.company_currency_id.rounding) == 0)
+
+        if any(expense.state not in ['draft', 'request'] or expense.sheet_id for expense in expenses_with_amount):
+            raise UserError(_("You cannot report twice the same line!"))
+        if not expenses_with_amount:
+            raise UserError(_("You cannot report the expenses without amount!"))
+        if len(expenses_with_amount.mapped('employee_id')) != 1:
+            raise UserError(_("You cannot report expenses for different employees in the same report."))
+        if any(not expense.product_id for expense in expenses_with_amount):
+            raise UserError(_("You can not create report without category."))
+        if len(self.company_id) != 1:
+            raise UserError(_("You cannot report expenses for different companies in the same report."))
+
+        # Check if two reports should be created
+        own_expenses = expenses_with_amount.filtered(lambda x: x.payment_mode == 'own_account')
+        company_expenses = expenses_with_amount - own_expenses
+        create_two_reports = own_expenses and company_expenses
+
+        sheets = [own_expenses, company_expenses] if create_two_reports else [expenses_with_amount]
+        values = []
+
+        # We use a fallback name only when several expense sheets are created,
+        # else we use the form view required name to force the user to set a name
+        for todo in sheets:
+            paid_by = 'company' if todo[0].payment_mode == 'company_account' else 'employee'
+            sheet_name = _("New Expense Report, paid by %(paid_by)s", paid_by=paid_by) if len(sheets) > 1 else False
+            if len(todo) == 1:
+                sheet_name = todo.name
+            else:
+                dates = todo.mapped('date')
+                if False not in dates:  # If at least one date isn't set, we don't set a default name
+                    min_date = format_date(self.env, min(dates))
+                    max_date = format_date(self.env, max(dates))
+                    if min_date == max_date:
+                        sheet_name = min_date
+                    else:
+                        sheet_name = _("%(date_from)s - %(date_to)s", date_from=min_date, date_to=max_date)
+
+            vals = {
+                'company_id': self.company_id.id,
+                'employee_id': self[0].employee_id.id,
+                'name': sheet_name,
+                'expense_line_ids': [Command.set(todo.ids)],
+                'state': 'draft',
+            }
+            values.append(vals)
+        _logger.warning(values)
+        return values
+
+    def action_submit_expenses(self):
+        self.ensure_one()
+        if self._context.get('forced_create'):
+            context_vals = self._get_default_expense_sheet_values()
+            sheets = self.env['hr.expense.sheet'].create(context_vals)
+            return {
+                'name': _('New Expense Reports'),
+                'type': 'ir.actions.act_window',
+                'views': [[False, "list"], [False, "form"]],
+                'res_model': 'hr.expense.sheet',
+                'domain': [('id', 'in', sheets.ids)],
+                'context': self.env.context,
+            }
+        else:
+            return super(HrExpense, self).action_submit_expenses()
